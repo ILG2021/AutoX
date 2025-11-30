08efa69e39a269670aa87682e6476c5207b7d641
luke <test@gmail.com>
2025-11-30 17:55:48 +0000

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,9 @@ android {
         resourceConfigurations.addAll(
             listOf("zh-rCN", "en", "es", "ar", "ja", "zh_TW", "fr", "de", "it", "ko", "ru", "tr", "lt")
         )
+        ndk {
+            abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a"))
+        }
     }
     buildFeatures {
         compose = true
@@ -50,15 +53,15 @@ android {
         // Configures multiple APKs based on ABI.
         abi {
             // Enables building multiple APKs per ABI.
-            isEnable = true
+            isEnable = false
             // By default all ABIs are included, so use reset() and include to specify that we only
             // want APKs for x86 and x86_64.
             // Resets the list of ABIs that Gradle should create APKs for to none.
             reset()
             // Specifies a list of ABIs that Gradle should create APKs for.
-            include("arm64-v8a")
+            include("arm64-v8a", "armeabi-v7a")
             // Specifies that we do not want to also generate a universal APK that includes all ABIs.
-            isUniversalApk = false
+            isUniversalApk = true
         }
     }
     val signing =
